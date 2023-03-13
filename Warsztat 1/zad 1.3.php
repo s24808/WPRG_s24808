2994c7d4301f2cf2b8939ca344bab6033f9e732d
s24808 <93827195+s24808@users.noreply.github.com>
2023-03-13 10:52:28 +0100

    Create zad 1.3.php

---
+++ Warsztat 1/zad 1.3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Square Root Calculator</title>
+</head>
+<body>
+    <?php
+    $a = pi();
+    $b = sqrt($a);
+    echo number_format($b, 2);
+    ?>
+</body>
+</html>
