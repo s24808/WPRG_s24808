e2222fe64f11401de51beb877cdb524de4a580b1
s24808 <93827195+s24808@users.noreply.github.com>
2023-04-27 19:53:35 +0200

    Create funkcje.php

---
+++ Warsztat 3/zad1/funkcje.php
@@ -0,0 +1,27 @@
+<?php
+
+function dodaj($a, $b)
+{
+    return $a + $b;
+}
+
+function odejmij($a, $b)
+{
+    return $a - $b;
+}
+
+function pomnoz($a, $b)
+{
+    return $a * $b;
+}
+
+function podziel($a, $b)
+{
+    if ($b == 0) {
+        return "Nie mozna dzielic przez zero";
+    } else {
+        return $a / $b;
+    }
+}
+
+?>
